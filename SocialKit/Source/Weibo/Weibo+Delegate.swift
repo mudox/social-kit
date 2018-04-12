2843326c676eb7f6077fb1fb2e01b9f77c0ad4db
mudox <imudox@gmail.com>
2018-04-12 09:54:23 +0800

    Add `TencenOAuth.getLastErrorMsg` output

---
+++ SocialKit/Source/Weibo/Weibo+Delegate.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension Weibo: WeiboSDKDelegate {
+  
+  public func didReceiveWeiboRequest(_ request: WBBaseRequest!) {
+    fatalError("Unimplemented")
+  }
+
+  public func didReceiveWeiboResponse(_ response: WBBaseResponse!) {
+    fatalError("Unimplemented")
+  }
+
+}
