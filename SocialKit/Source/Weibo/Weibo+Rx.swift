193d8ac55b8ac6ea65b352be999aba4dc0d1f15c
mudox <imudox@gmail.com>
2018-06-26 20:45:06 +0800

    Add `Weibo+Rx.swift`

---
+++ SocialKit/Source/Weibo/Weibo+Rx.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+import JacKit
+fileprivate let jack = Jack.fileScopeInstance().setLevel(.verbose)
+
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: Weibo {
+  
+  public static var signIn: Single<Weibo.SignInResult> {
+    return .create { single -> Disposable in
+      Base.signIn(completion: { result, error in
+        if let e = error {
+          single(.error(e))
+        } else if let result = result {
+          if let r = result as? Weibo.SignInResult {
+            jack.info("\(r)")
+            single(.success(r))
+          } else {
+            single(.error(SocialKitError.sdk(reason: "casting `result` to `Weibo.SignInResult` failed")))
+          }
+        } else {
+          single(.error(SocialKitError.sdk(reason: "`result` & `error` should not both be nil")))
+        }
+      })
+      return Disposables.create()
+    } // return .create
+  } // func signIn
+  
+}
